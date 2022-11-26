2d2ba935d80acbd3d2a6addc89351a83b9ab9f6f
thanhnt <nguyentruongthanh0612@gmail.com>
2022-11-26 15:07:24 +0700

    fix bug

--- src/Component/LoginFacebook/LoginFacebook.jsx
+++ src/Component/LoginFacebook/LoginFacebook.jsx
@@ -1,25 +1,25 @@
-import React from "react";
-import FacebookLogin from "react-facebook-login";
-import { useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import { loginFacebookApi } from "../../redux/reducer/userReducer";
+// import React from "react";
+// import FacebookLogin from "react-facebook-login";
+// import { useDispatch } from "react-redux";
+// import { useNavigate } from "react-router-dom";
+// import { loginFacebookApi } from "../../redux/reducer/userReducer";
 
-export default function LoginFacebook() {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const responseFacebook = async (response) => {
-    console.log(response);
-    const action = loginFacebookApi(response.accessToken);
-    await dispatch(action);
-    navigate("/home");
-  };
-  return (
-    <div className="loginFB">
-      <FacebookLogin
-        appId="1522839604888257"
-        fields="name,email,picture,phone"
-        callback={responseFacebook}
-      />
-    </div>
-  );
-}
+// export default function LoginFacebook() {
+//   const dispatch = useDispatch();
+//   const navigate = useNavigate();
+//   const responseFacebook = async (response) => {
+//     console.log(response);
+//     const action = loginFacebookApi(response.accessToken);
+//     await dispatch(action);
+//     navigate("/home");
+//   };
+//   return (
+//     <div className="loginFB">
+//       <FacebookLogin
+//         appId="1522839604888257"
+//         fields="name,email,picture,phone"
+//         callback={responseFacebook}
+//       />
+//     </div>
+//   );
+// }
